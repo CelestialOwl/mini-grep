7611c785ae7395d09c3dcd57a5edf1ec50f08901
hassan <hassan.akhtar44@yahoo.com>
2023-10-30 11:07:56 +0500

    implemented the iterator and case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,26 @@
-use std::fs;
+use std::{fs, env};
 use std::error::Error;
 
 pub struct Config {
    pub query: String,
-  pub  file_path: String
+  pub  file_path: String,
+  pub ignore_case:bool,
 }
 
 impl Config {
-   pub fn build (args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-                return Err("not enough parameters");
-        }
+   pub fn build (mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
         
-        let query : String = args[1].clone();
-        let file_path : String = args[2].clone();
-        Ok(Config {query, file_path})
+        let query : String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {query, file_path, ignore_case})
     }
     
 }
@@ -31,13 +37,12 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    contents.lines().filter(|line|line.contains(&query)).collect()
 }
 
 #[cfg(test)]
